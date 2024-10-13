79dea1b5ca6ae6fd3be48edf83ec833f143087fe
Mohammed Ghauri <109049050+GhauriM@users.noreply.github.com>
2024-10-12 23:13:21 -0400

    uploaded bootcamp files

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+            System.out.println("M   M   GGG ");
+            System.out.println("MM MM  G   G");
+            System.out.println("M M M  G    ");
+            System.out.println("M   M  G GGG");
+            System.out.println("M   M  G   G");
+            System.out.println("M   M  G   G");
+            System.out.println("M   M   GGG ");
+
+        }
+}
---
+++ section two/WholeNumbers.java
@@ -0,0 +1,11 @@
+public class WholeNumbers{
+    
+    public static void main(String[] args) {
+        
+        long globalPopulation = 80000000000L;
+        System.out.println("the global population is " + globalPopulation);
+        long dailyGoogleSearches = 85000000000L;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches.");
+
+    }
+}
